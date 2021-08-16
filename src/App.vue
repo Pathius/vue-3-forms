29d4c38c38e56422e45ab6dffc9893cc0b23de97
Bartosz Urbanski <bartoszurbanski.biznes@gmail.com>
2021-08-16 21:30:47 +0200

    Implemented very basic version of Input

--- src/App.vue
+++ src/App.vue
@@ -1,2 +1,27 @@
 <template>
+  <Input
+    v-model="form.name"
+    placeholder="Please insert your name"
+    label="name"
+    type="text"
+  />
+  {{ form.name }}
 </template>
+
+<script>
+import { ref } from 'vue'
+import Input from './components/Input.vue'
+
+export default {
+  components: {
+    Input
+  },
+  setup () {
+    const form = ref({
+      name: ''
+    })
+
+    return { form }
+  }
+}
+</script>
---
+++ src/components/Input.vue
@@ -0,0 +1,33 @@
+<template>
+  <label>
+    {{ label }}
+  </label>
+  <!-- If type is not declared HTML will automatically make input type="text" -->
+  <!-- If placeholder is not declined, label will be used as placeholder -->
+  <input
+    v-bind="$attrs"
+    :placeholder="placeholder || label"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+  />
+</template>
+
+<script>
+// TODO Create example of a modifier
+export default {
+  props: {
+    label: {
+      type: String,
+      default: ''
+    },
+    placeholder: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: [String, Number],
+      default: ''
+    }
+  }
+}
+</script>
