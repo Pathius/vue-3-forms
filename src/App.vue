c61f7a1a814ddc7091ca1f21ee6c292e3c945ca3
Bartosz Urbanski <bartoszurbanski.biznes@gmail.com>
2021-08-17 19:56:04 +0200

    Implement Checkbox into App

--- src/App.vue
+++ src/App.vue
@@ -10,14 +10,20 @@
     label="Country"
     :options="countries"
   />
+  <Checkbox
+    v-model="isAdult"
+    label="Is adult?"
+  />
 </template>
 
 <script setup>
-import { ref } from 'vue';
-import Input from './components/Input.vue';
+import { ref } from 'vue'
+import Input from './components/Input.vue'
 import Select from './components/Select.vue'
+import Checkbox from './components/Checkbox.vue'
 
 const name = ref('');
 const countries = ['Poland', 'Germany', 'USA']
 const selectedCountry = ref('')
+const isAdult = ref(false)
 </script>
