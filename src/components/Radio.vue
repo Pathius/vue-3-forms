60785c47adb36d839e780e12a37d3d300288be05
Bartosz Urbanski <bartoszurbanski.biznes@gmail.com>
2021-08-18 20:49:38 +0200

    Created base Radio component

---
+++ src/components/Radio.vue
@@ -0,0 +1,38 @@
+<template>
+  <input
+    :id="id || label"
+    type="radio"
+    :checked="modelValue === value"
+    :value="value"
+    @change="$emit('update:modelValue', value)"
+  >
+  <label 
+    v-if="label"
+    :for="id || label"
+  >
+    {{ label }}
+  </label>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      default: ''
+    },
+    label: {
+      type: String,
+      default: ''
+    },
+    modelValue: {
+      type: [String, Number],
+      default: ''
+    },
+    value: {
+      type: [String, Number],
+      required: true
+    }
+  }
+}
+</script>
