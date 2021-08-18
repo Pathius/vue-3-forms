27f8f8d138429f51c7260dd8eb0b5ec6cfeb95f3
Bartosz Urbanski <bartoszurbanski.biznes@gmail.com>
2021-08-18 20:49:48 +0200

    Implemented it into App

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,21 @@
     v-model="isAdult"
     label="Is adult?"
   />
+  <h2>
+    Do you like dogs?
+  </h2>
+  <Radio 
+    v-model="likeDogs"
+    :value="1"
+    label="Yes"
+    name="likedogs"
+  />
+  <Radio 
+    v-model="likeDogs"
+    :value="0"
+    label="No"
+    name="likedogs"
+  />
 </template>
 
 <script setup>
@@ -21,9 +36,11 @@ import { ref } from 'vue'
 import Input from './components/Input.vue'
 import Select from './components/Select.vue'
 import Checkbox from './components/Checkbox.vue'
+import Radio from './components/Radio.vue'
 
 const name = ref('');
 const countries = ['Poland', 'Germany', 'USA']
 const selectedCountry = ref('')
 const isAdult = ref(false)
+const likeDogs = ref(1)
 </script>
