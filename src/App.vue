8ec5704bcdb1c762aa41c545675a0df4a27bc8af
Bartosz Urbanski <bartoszurbanski.biznes@gmail.com>
2021-08-20 20:02:13 +0200

    Created todo list

--- src/App.vue
+++ src/App.vue
@@ -24,7 +24,22 @@
   />
 </template>
 
+// TODO: Implement reset.css file, provide font, title, favicon etc.
+// TODO: Cleanup existing project structure
+// TODO: Create new form component where there will be separate sections for individual data
+// TODO: Section 1 will cover basic informations like name, email, age, country
+// TODO: Improve styling for Input and Select
+// TODO: Create RadioCard component to combine Radio button and icon
+// TODO: Section 2 will cover gender using RadioCard component
+// TODO: Create CheckboxCard to combine Checkbox button and icon
+// TODO: Section 3 will cover favorite food
+// TODO: Create Uploader component
+// TODO: Section 4 will cover uploader component so user can upload some cute photos
+// TODO: Implement validation with some animations
+// TODO: Create UserSummary component to display all data gathered
+// TODO: Create skeleton version of UserSummary
 // TODO: Provide name attribute for single each component
+// TODO: Make sure you're following rules of accessibility
 <script setup>
 import { ref } from 'vue'
 import Input from './components/Input.vue'
