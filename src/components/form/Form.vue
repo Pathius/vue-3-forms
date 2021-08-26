d9143f334fd985592bd376bc8f86c732b8df8dfd
Bartosz Urbanski <bartoszurbanski.biznes@gmail.com>
2021-08-26 19:51:18 +0200

    Implemented Vue structure for basic form content

---
+++ src/components/form/Form.vue
@@ -0,0 +1,45 @@
+<template>
+  <form class="form">
+    <fieldset class="form__fieldset">
+      <legend class="form__legend">Basic information</legend>
+      <Input
+        v-model="form.fullName"
+        label="Full name"
+        placeholder="Wayne Fisher"
+      />
+      <Input
+        type="email"
+        v-model="form.email"
+        label="Email"
+        placeholder="wayne@gmail.com"
+      />
+      <Input 
+        type="date"
+        v-model="form.birthdate"
+        label="Birthdate" 
+      />
+      <Select
+        v-model="form.favouriteAnimal"
+        :options="animals"
+        label="Favourite animal"
+      />
+    </fieldset>
+  </form>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import Input from '../Input.vue';
+import Select from '../Select.vue';
+
+const form = ref({
+  fullName: '',
+  email: '',
+  birthdate: '',
+  favouriteAnimal: '',
+});
+
+const animals = ['Dog', 'Cat', 'Parrot', 'Tiger'];
+</script>
+
+<style scoped lang="scss" src="./form.scss" />
